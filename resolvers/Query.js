4acc72734bb8b69f70f0f88e1441c06fdcee5795
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2018-12-30 17:55:19 +0100

    setup query to fetch a single post

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -5,6 +5,14 @@ const Query = {
   getAllPost: async (_, args, ctx, info) => {
     const posts = await Post.findAll();
     return posts
+  },
+  getPostById: async (_, { id }, ctx, info) => {
+    const singlePost = await Post.findOne({
+      where: {
+        id
+      }
+    })
+    return singlePost
   }
 }
 
