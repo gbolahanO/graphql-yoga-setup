2d401174a20d8481c52c6ac92126fbda5df2c8e1
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2018-12-30 15:48:15 +0100

    refactored code

--- src/server.js
+++ src/server.js
@@ -2,21 +2,16 @@ require('dotenv').config({
   path: './variables.env'
 })
 import { GraphQLServer } from 'graphql-yoga'
-
-const typeDefs = `
-  type Query {
-    hello: String!
-  }
-`
+import Query from '../resolvers/Query'
+import Mutation from '../resolvers/Mutation'
 
 const resolvers = {
-  Query: {
-    hello: () => "Hello World"
-  }
+  Query,
+  Mutation
 }
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers
 })
 
