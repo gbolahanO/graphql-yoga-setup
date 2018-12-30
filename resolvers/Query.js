a81a4edcd0b633ad9e3d899eee507ad769f3f4c9
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2018-12-30 17:50:09 +0100

    imported post and user model into query resolver and added Date as a scalar type in schema

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,5 +1,11 @@
+import User from '../models/User'
+import Post from '../models/Post'
+
 const Query = {
-  
+  getAllPost: async (_, args, ctx, info) => {
+    const posts = await Post.findAll();
+    return posts
+  }
 }
 
 export default Query
