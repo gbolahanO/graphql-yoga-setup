7f75cd9766c3eaf4390eaab46497b47e066f3af6
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2019-02-28 21:33:11 +0100

    code refactor and added hello query

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -1,13 +1,5 @@
-import User from '../models/User'
-import Post from '../models/Post'
 const Mutation = {
-  createPost: async (_, { data }, ctx, info) => {
-    let post = await Post.create(data);
-    return post;
-  },
-  createUser: (_, args, ctx, info) => {
-    console.log(args)
-  }
+
 }
 
 export default Mutation
--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,18 +1,6 @@
-import User from '../models/User'
-import Post from '../models/Post'
-
 const Query = {
-  getAllPost: async (_, args, ctx, info) => {
-    const posts = await Post.findAll();
-    return posts
-  },
-  getPostById: async (_, { id }, ctx, info) => {
-    const singlePost = await Post.findOne({
-      where: {
-        id
-      }
-    })
-    return singlePost
+  hello(_, args, ctx, info) {
+    return "hello"
   }
 }
 
